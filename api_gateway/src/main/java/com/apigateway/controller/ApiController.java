23e0bf7e330638e64d9a84014a79d3efb8727d3c
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-07 01:02:08 +0530

    properties reading from local files

---
+++ api_gateway/src/main/java/com/apigateway/controller/ApiController.java
@@ -0,0 +1,19 @@
+//package com.apigateway.controller;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/v1/controller")
+//public class ApiController {
+//
+//    @GetMapping
+//    public ResponseEntity<?> test(){
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .header("Content-Type", "application/json")
+//                .body("Welcome\n to\n API_GATEWAY");
+//    }
+//}
